e021beb430b6db32e1ee68fcc6efb4fdb15ea4e8
Peter Connelly <pconnelly898@gmail.com>
2020-09-20 23:04:27 -0400

    changing out hard-coded token for testing

--- frontend/src/graphql/index.ts
+++ frontend/src/graphql/index.ts
@@ -7,9 +7,8 @@ const httpLink = new HttpLink({ uri: "http://localhost:8080/graphql", fetch});
 console.log("running the file");
 const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
-    // const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
     // return the headers to the context so httpLink can read them
-    const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJNeVRlc3RVc2VyIiwiZW1haWwiOiJ0ZXN0dXNlckB0ZXN0LmNvbSIsInJvbGVzIjoidXNlciIsImlkIjo4fQ.NtcMsZR1Xm0h7Hdoush5_B2PJuiW3wLKp45se7cXBLENWgoLzg919oxUbEd_kM0MkZCI6pnYylMI6vwUmG5mUA";
     console.log(localStorage.getItem("token"));
     return {
       headers: {
